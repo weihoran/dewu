088fe3f98dcb9240d52114fb1c253a2a8961f58f
horan <442460904@qq.com>
2021-06-02 20:36:22 +0800

    Update identification.js

--- src/main/resources/static/js/identification.js
+++ src/main/resources/static/js/identification.js
@@ -7,6 +7,7 @@ $(function() {
 
     $.get("/api/get/identification/" + number, function(data, status){
         $("#productName").html(data.productName)
+        $("title").html(data.productName + "鉴定报告")
         list = data.imagelist
         for (i = 0; i < list.length; i++) {
               $.get("/api/get/file/" + list[i], function(data, status){
